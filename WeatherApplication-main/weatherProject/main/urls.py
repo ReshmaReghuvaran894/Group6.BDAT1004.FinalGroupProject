d7326a23ef09710e59c923d7a16843871207997f
ReshmaReghuvaran894 <115848991+ReshmaReghuvaran894@users.noreply.github.com>
2022-12-16 19:13:14 -0500

    Add files via upload

---
+++ WeatherApplication-main/weatherProject/main/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('chart/', views.chart),
+    path('humidity_chart/', views.humidity_chart),
+    path('get_by_id', views.get_by_id),
+    path('get_all_elements', views.get_all_elements),
+    path('get_by_id', views.get_by_id),
+    path('get_all_pollutants', views.get_all_pollutants),
+    path('pollution_index', views.pollution_index),
+]
